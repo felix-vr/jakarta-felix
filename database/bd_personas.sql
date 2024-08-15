d3a9c7e27d38b18dbaeeb16e320fa8a57990d808
felix.vr <felix.vr@patzcuaro.tecnm.mx>
2024-08-15 13:36:20 -0600

    Almost finish

---
+++ database/bd_personas.sql
@@ -0,0 +1,20 @@
+-- Create the database and connect to it.
+connect 'jdbc:derby://localhost:1527/studentsdb;create=true';
+
+-- Add a user to the database, username dev, password dev
+CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.user.dev','dev');
+
+-- Grant all privileges to user dev
+CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.database.fullAccessUsers','dev');
+
+-- Disconnect from the newly created database
+disconnect;
+
+-- Reconnect to the newly created database as user dev
+connect 'jdbc:derby://localhost:1527/studentsdb;user=dev;password=dev';
+
+-- Create tables
+CREATE TABLE STUDENTS (STUDENT_ID INT PRIMARY KEY, FIRST_NAME VARCHAR(20), LAST_NAME VARCHAR(20), EMAIL VARCHAR(30));
+insert into STUDENTS values 
+	(1,'Denisse','Velazquez','dvm@outlook.com'),
+	(2,'Felix','Villagomez','fvr@outlook.com');
